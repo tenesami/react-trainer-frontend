0fafaa0b1651006231a3f5620a6782f7c9b9cc16
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-01-30 12:21:02 -0700

    first commit with basic setups

---
+++ src/Reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import trainerReducer from './trainerReducer';
+
+const rootReducer = combineReducers({
+    trainerReducer
+})
+
+export default rootReducer;
---
+++ src/Reducers/trainerReducer.js
@@ -0,0 +1,6 @@
+export default (state={trainer:[], loading: false}, action) => {
+    switch(action.type){
+        default: 
+        return state;
+    }
+}
