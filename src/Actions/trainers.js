e73c84bc63f814e8e184a2cd763316427b06d884
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-02-01 06:13:43 -0700

    fetch trainer data from backend

--- src/Actions/trainers.js
+++ src/Actions/trainers.js
@@ -1,6 +1,8 @@
 export const getTrainers = () => {
     return (dispach) => {
         dispach ({type: "LODING_TRAINERS"})
+        fetch('/trainer')
+        .then(res => res.json())
+        .then(trainers => dispach({type: "FETCH_TRAINERS", payload: trainers }))
     }
-
 }
